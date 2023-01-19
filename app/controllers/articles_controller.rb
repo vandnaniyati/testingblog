35d69e5a25a58f31c327994e42e830a5d3835251
vandna <vandna@niyati.com>
2023-01-19 19:52:25 +0530

    add Crud in article controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,47 @@
 class ArticlesController < ApplicationController
+  before_action :set_articles, :only[:show, :edit, :update, :destroy]
+  def index
+    @articles = Article.all
+  end
+
+  def show
+  end
+
+  def new
+   @article = Article.new
+  end
+
+  def create
+    if @article.save
+      redirect_to article_path notice: "article create succesfully"
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @article.update(article_params)
+      redirect_to article_path notice: "article update succesfully"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to @article_path
+  end
+
+  private
+
+  def set_articles
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
