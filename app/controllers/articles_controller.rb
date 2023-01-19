1f072bf8612a35e6828e69bfb7c900950f4de6b9
vandna <vandna@niyati.com>
2023-01-19 20:18:47 +0530

    add routes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :set_articles, :only[:show, :edit, :update, :destroy]
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
   def index
     @articles = Article.all
   end
@@ -37,7 +37,7 @@ class ArticlesController < ApplicationController
 
   private
 
-  def set_articles
+  def set_article
     @article = Article.find(params[:id])
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+ root 'pages#home'
+ get 'home', to: 'pages#home'
+ get 'about', to: 'pages#about'
+ 
+ resources :articles #only: [:show]
 end
